862aecbbe6f4f69508de2b0b7898d7f7ced242c8
Shaomeng Zhang <randomor@gmail.com>
2019-05-30 13:14:18 -0500

    Fixing commented out specs

--- spec/controllers/authorizations_controller_spec.rb
+++ spec/controllers/authorizations_controller_spec.rb
@@ -12,7 +12,7 @@ describe Devise::Oauth2Providable::AuthorizationsController do
         get :new, params: {:client_id => client.identifier, :redirect_uri => redirect_uri, response_type: 'code' }
       end
       it { should respond_with :success }
-      # it { should respond_with_content_type :html }
+      it { response.content_type.should eq 'text/html' }
       it { should assign_to(:redirect_uri).with(redirect_uri) }
       it { should assign_to(:response_type) }
       it { should render_template 'devise/oauth2_providable/authorizations/new' }
@@ -26,7 +26,7 @@ describe Devise::Oauth2Providable::AuthorizationsController do
         get :new, params: { :client_id => client.identifier, :redirect_uri => redirect_uri, response_type: 'code' }
       end
       it { should respond_with :bad_request }
-      # it { should respond_with_content_type :html }
+      it { response.content_type.should eq 'text/html' }
     end
   end
 end
